7ff6660f881fb731c464c4ac3920a8a315aaf951
Théo Ballerini <75840013+TheoBall@users.noreply.github.com>
2023-06-13 09:23:29 +0200

    commentaires classe segment

--- app/src/main/java/com/example/snake_game/SnakeSegment.java
+++ app/src/main/java/com/example/snake_game/SnakeSegment.java
@@ -1,6 +1,5 @@
 package com.example.snake_game;
 
-import android.content.Context;
 import android.widget.ImageView;
 
 public class SnakeSegment {
@@ -8,38 +7,68 @@ public class SnakeSegment {
     private float previousY;
     private ImageView segmentImage;
 
+    /**
+     * Constructeur de Segment
+     * @param image image du segment
+     */
     public SnakeSegment(ImageView image) {
         this.segmentImage = image;
     }
 
+    /**
+     * @return la précédante position X
+     */
     public float getPreviousX() {
         return previousX;
     }
 
+    /**
+     * @return la précédante position Y
+     */
     public float getPreviousY() {
         return previousY;
     }
 
+    /**
+     * Change la valeur X de l'image du segment
+     * @param newX valeur X qui remplacera l'ancienne
+     */
     public void setX(float newX) {
         segmentImage.setX(newX);
     }
 
+    /**
+     * Change la valeur Y de l'image du segment
+     * @param newY valeur Y qui remplacera l'ancienne
+     */
     public void setY(float newY) {
         segmentImage.setY(newY);
     }
 
+    /**
+     * @return la valeur X de l'image du segment
+     */
     public float getX() {
         return segmentImage.getX();
     }
 
+    /**
+     * @return la valeur Y de l'image du segment
+     */
     public float getY() {
         return segmentImage.getY();
     }
 
+    /**
+     * @return la largeur de l'image du segment
+     */
     public float getWidth() {
         return segmentImage.getWidth();
     }
 
+    /**
+     * @return la hauteur de l'image du segment
+     */
     public float getHeight() {
         return segmentImage.getHeight();
     }
